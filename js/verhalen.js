66d9df62d7edee95481aeef551bae03d95a7a489
Joycemulder <34276313+Joycemulder@users.noreply.github.com>
2018-01-30 14:16:01 +0100

    verhaal nummer 3 gemaakt

--- js/verhalen.js
+++ js/verhalen.js
@@ -5,3 +5,25 @@ function sterToggle() {
     this.classList.toggle('fullster');
 }
 document.getElementById('leegster').addEventListener('click', sterToggle);
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
